656a7d6477268f9785b41ecaff78d89e920962f5
Jacob-Polane <159529443+Jacob-Polane@users.noreply.github.com>
2024-03-11 15:32:55 +0200

    ✨ landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,87 @@
-import Image from "next/image";
+import React from 'react';
+import { Col, Row,Button } from 'antd';
+import menu from '../../public/menu.png';
+import home from '../../public/home.jpg';
 import styles from "./page.module.css";
+import Image from 'next/image';
+import Logo from '../../public/Logo.jpg'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-     
-    </main>
+  
+  <>
+    <Row className={styles.row}>
+        <Col 
+          className={styles.logo}
+          key='logo'
+          xs={{ flex: '100%' }}
+          sm={{ flex: '50%' }}
+          md={{ flex: '40%' }}
+          lg={{ flex: '20%' }}
+          xl={{ flex: '15%' }}>
+
+         <Image className={styles.logoImage} src={Logo} alt='logo'/>
+        </Col>
+
+        <Col
+          className={styles.nav}
+          key='explore'
+          xs={{ flex: '100%' }}
+          sm={{ flex: '50%' }}
+          md={{ flex: '40%' }}
+          lg={{ flex: '20%' }}
+          xl={{ flex: '15%' }}
+        >
+
+          Explore
+        </Col>
+
+        <Col
+          className={styles.nav}
+          key='search'
+          xs={{ flex: '100%' }}
+          sm={{ flex: '50%' }}
+          md={{ flex: '40%' }}
+          lg={{ flex: '20%' }}
+          xl={{ flex: '15%' }}
+        >
+
+          Search
+        </Col>
+
+        
+        <Col
+          className={styles.nav}
+          key='about'
+          xs={{ flex: '100%' }}
+          sm={{ flex: '50%' }}
+          md={{ flex: '40%' }}
+          lg={{ flex: '20%' }}
+          xl={{ flex: '15%' }}
+        >
+
+          About
+        </Col>
+        <Image  className={styles.menu} src={menu} alt='menu'/> 
+    </Row>
+
+    <Row>
+      <Col 
+      className={styles.homeMessage}
+      span={12}>
+        <h1 className={styles.homeMessageh1}>Welcome to BIL</h1>
+        <p className={styles.homeMessageParagraph} >
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, aut natus impedit incidunt explicabo velit culpa!
+          Nam sit officiis repellendus illo pariatur magnam. Earum unde recusandae eos tempore, quis illo! Lorem, ipsum dolor
+           sit amet consectetur adipisicing elit. Deleniti et laboriosam at alias dignissimos dolores autem maxime id ducimus 
+           nesciunt quasi cum inventore nisi, eum neque voluptates natus, expedita quidem.
+        </p>
+        <Button className={styles.loginButton}>Login</Button>
+      </Col>
+      <Col span={12}>
+        <Image className={styles.homeImage} src={home} alt=''/>
+      </Col>
+    </Row>
+  </>
   );
 }
---
+++ client/src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+import React,{FC} from 'react';
+const Signup: React.FC = () => {
+
+    return (
+        <h1>Signup</h1>
+    );
+}
+
+export default Signup;
