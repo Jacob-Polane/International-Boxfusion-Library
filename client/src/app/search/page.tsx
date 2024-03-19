a160f624c6190950d6b51dc05f2a44fba310c3b1
Jacob-Polane <159529443+Jacob-Polane@users.noreply.github.com>
2024-03-19 13:55:23 +0200

    ✨ added auth guard

--- client/src/app/search/page.tsx
+++ client/src/app/search/page.tsx
@@ -3,6 +3,7 @@ import React,{FC,useState,useEffect}from 'react';
 import {Form,type FormProps,Button,Input,List,message,Avatar} from 'antd';
 import VirtualList from 'rc-virtual-list';
 import NavBar from '@/components/navbar/NavBar';
+import AuthGuard from '@/components/authGuard/AuthGuard';
 
 type FieldType = {
   isbn?: string;
@@ -58,44 +59,46 @@ const Search: FC =()=>{
   };
 
     return(
-    <div >
-       <NavBar/>
-        <Form layout='inline'  style={{width:'100%',justifyContent:'center',marginTop:'20px'}} onFinish={onFinish}>
-           <Form.Item <FieldType> label="ISBN" name="isbn">
-            <Input  placeholder="Search by ISBN" style={{width:200}}/>
-          </Form.Item>
-          <Form.Item <FieldType> label="Title" name="title">
-            <Input  placeholder="Search by title" style={{width:200}} />
-          </Form.Item>
-          <Form.Item <FieldType> label="Author" name="author">
-            <Input placeholder="Search by Author" style={{width:200}} />
-          </Form.Item>
-          <Form.Item  wrapperCol={{ span: 8, offset: 0 }}>
-            <Button type="primary" htmlType='submit'>Search</Button>
-          </Form.Item>
-        </Form>
-        <List style={{width:'100%',alignItems:'center',padding:20,display:'flex',justifyContent:'center',}}>
-          <h1 style={{color:'#1BA1E2'}}>Results:</h1>
-          <VirtualList
-            data={data}
-            height={ContainerHeight}
-            itemHeight={60}
-            itemKey="email"
-            onScroll={onScroll}
-            style={{border:'4px solid #1BA1E2',padding:40,minWidth:1000,minHeight:500,borderRadius:4}}
-          >
-            {(item: UserItem) => (
-              <List.Item key={item.email}>
-                <List.Item.Meta
-                  avatar={<Avatar src={item.picture.large} />}
-                  title={<a href="https://ant.design">{item.name.last}</a>}
-                  description={item.email}
-                />
-              </List.Item>
-            )}
-          </VirtualList>
-      </List>
-    </div>
+  <AuthGuard>
+      <div >
+         <NavBar/>
+          <Form layout='inline'  style={{width:'100%',justifyContent:'center',marginTop:'20px'}} onFinish={onFinish}>
+             <Form.Item <FieldType> label="ISBN" name="isbn">
+              <Input  placeholder="Search by ISBN" style={{width:200}}/>
+            </Form.Item>
+            <Form.Item <FieldType> label="Title" name="title">
+              <Input  placeholder="Search by title" style={{width:200}} />
+            </Form.Item>
+            <Form.Item <FieldType> label="Author" name="author">
+              <Input placeholder="Search by Author" style={{width:200}} />
+            </Form.Item>
+            <Form.Item  wrapperCol={{ span: 8, offset: 0 }}>
+              <Button type="primary" htmlType='submit'>Search</Button>
+            </Form.Item>
+          </Form>
+          <List style={{width:'100%',alignItems:'center',padding:20,display:'flex',justifyContent:'center',}}>
+            <h1 style={{color:'#1BA1E2'}}>Results:</h1>
+            <VirtualList
+              data={data}
+              height={ContainerHeight}
+              itemHeight={60}
+              itemKey="email"
+              onScroll={onScroll}
+              style={{border:'4px solid #1BA1E2',padding:40,minWidth:1000,minHeight:500,borderRadius:4}}
+            >
+              {(item: UserItem) => (
+                <List.Item key={item.email}>
+                  <List.Item.Meta
+                    avatar={<Avatar src={item.picture.large} />}
+                    title={<a href="https://ant.design">{item.name.last}</a>}
+                    description={item.email}
+                  />
+                </List.Item>
+              )}
+            </VirtualList>
+        </List>
+      </div>
+      </AuthGuard>
     );
 }
 
